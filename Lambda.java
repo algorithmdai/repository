78f44f85ecc1627191bab2842bcc5ea3ef0f4730
algorithmdai <98253434+algorithmdai@users.noreply.github.com>
2022-02-05 14:47:33 +0800

    Add files via upload

---
+++ Lambda.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Lambda {
+    /*
+Lambda表达式   函数式编程思想
+              体验 Lambda表达式
+              Lambda表达式格式
+              Lambda表达式的前提
+              练习
+              省略格式
+              注意事项
+              与匿名内部类的区别
+ */
+}
